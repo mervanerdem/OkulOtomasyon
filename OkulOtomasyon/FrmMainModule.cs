9cc7f5ca39114254e15eae19e85aa0a43f62f75a
Fincore <Fincore@DESKTOP-NEIDSN5>
2022-06-15 11:48:23 +0300

    Sql problem suspended.Proje dosyası ekle.

---
+++ OkulOtomasyon/FrmMainModule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OkulOtomasyon
+{
+    public partial class FrmMainModule : Form
+    {
+        public FrmMainModule()
+        {
+            InitializeComponent();
+        }
+
+        private void wizardControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem2_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem3_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem4_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem5_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+
+        }
+
+        private void barButtonItem6_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        FrmTeachers frm1;
+
+        private void BtnOgretmen_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            frm1 = new FrmTeachers();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+    }
+}
